4e0d2c7b9125fb3f203e66a3a0e3ddd1749c12c1
Mark OBrien <mark.obrien@blueearth.net>
2015-09-15 12:51:33 -0500

    L5

--- web/laravel/resources/views/flyers/create.blade.php
+++ web/laravel/resources/views/flyers/create.blade.php
@@ -1,5 +1,10 @@
+@inject('countries', 'App\Http\Utilities\Country')
 @extends('layout')
 
 @section('content')
 <h1>Selling Your Home?</h1>
+    <hr>
+    <form method="post" enctype="multipart/form-data" action="/flyers">
+        @include('flyers.form')
+    </form>
 @stop
